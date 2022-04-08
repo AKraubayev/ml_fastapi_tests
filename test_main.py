ba925814c06e9c9cc197005c295106f138e6c7b2
Amangeldi Kraubayev <95435745+AKraubayev@users.noreply.github.com>
2022-04-08 21:45:25 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -12,8 +12,7 @@ def test_read_main():
 
 def test_read_predict_positive():
     response = client.post("/predict/",
-                           json={"text": "I like machine learning!"}
-                          )
+                           json={"text": "I like machine learning!"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
